e040fcaca7d779637136c3ff28ba9a3ac32767e9
Angel <arn0005@alu.medac.es>
2022-03-21 10:52:54 +0100

    Inicio proyecto

---
+++ Fruteria/src/fruteria/Cliente.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fruteria;
+
+/**
+ *
+ * @author angel.ruiz
+ */
+public class Cliente {
+    private int numero;
+    
+}
