81acfdea3df9c8cb7b28a618640294f9468716f7
Angel <arn0005@alu.medac.es>
2022-03-21 11:04:06 +0100

    Commit inicial

--- src/elfondo/Cliente.java
+++ src/elfondo/Cliente.java
@@ -2,13 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package fruteria;
+package elfondo;
 
 /**
  *
  * @author angel.ruiz
  */
 public class Cliente {
-    private int numero;
     
 }
--- src/elfondo/Main.java
+++ src/elfondo/Main.java
@@ -2,19 +2,19 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package fruteria;
+package elfondo;
 
 /**
  *
  * @author angel.ruiz
  */
-public class Fruteria {
+public class Main {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        // TODO code application logic here
     }
     
 }
